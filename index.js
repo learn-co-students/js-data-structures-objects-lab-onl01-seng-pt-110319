35bddd4fcec8013b74683d19c308c41b6846bb9c
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 13:02:16 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function deleteFromDriverByKey(driver, key) {
   delete object[key];
   return object;
 }
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key];
+    return driver;
+
+}
